38d7ce9c5725d11961db8f04cdd8b0d1bdf51fa4
Aaron Brown <aaron@browncasa.net>
2019-09-02 21:54:38 -0500

    switched from os to pathlib + string representation

--- DjangoGirlsTutorial/settings.py
+++ DjangoGirlsTutorial/settings.py
@@ -10,12 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-import os
-# from pathlib import Path
+# import os
+from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# BASE_DIR = Path(__file__, '..', '..').resolve()
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__, '..', '..').resolve()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -58,8 +58,8 @@ ROOT_URLCONF = 'DjangoGirlsTutorial.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        # 'DIRS': [Path(BASE_DIR, 'templates')]
+        # 'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [str(Path(BASE_DIR, 'templates'))]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -82,7 +82,8 @@ WSGI_APPLICATION = 'DjangoGirlsTutorial.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': str(Path(BASE_DIR, 'db.sqlite3')),
     }
 }
 
@@ -124,4 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = str(Path(BASE_DIR / 'static'))

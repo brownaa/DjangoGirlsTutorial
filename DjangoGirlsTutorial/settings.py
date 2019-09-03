4c98859d6bd42fcf85d6a7e1b6687c808c9ea373
Aaron Brown <aaron@browncasa.net>
2019-09-02 21:44:33 -0500

    switched from pathlib to os... round 2

--- DjangoGirlsTutorial/settings.py
+++ DjangoGirlsTutorial/settings.py
@@ -58,8 +58,8 @@ ROOT_URLCONF = 'DjangoGirlsTutorial.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        # 'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        'DIRS': [Path(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        # 'DIRS': [Path(BASE_DIR, 'templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {

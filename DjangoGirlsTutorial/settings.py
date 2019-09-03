d0334730db483da579b74f8116f980bbfc09094c
Aaron Brown <aaron@browncasa.net>
2019-09-02 22:37:57 -0500

    final "initial" commit

--- DjangoGirlsTutorial/settings.py
+++ DjangoGirlsTutorial/settings.py
@@ -14,8 +14,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BASE_DIR = Path(__file__, '..', '..').resolve()
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = Path(__file__, '..', '..').resolve()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -58,8 +58,8 @@ ROOT_URLCONF = 'DjangoGirlsTutorial.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        # 'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        'DIRS': [str(Path(BASE_DIR, 'templates'))]
+        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        # 'DIRS': [str(Path(BASE_DIR, 'templates'))]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -82,8 +82,8 @@ WSGI_APPLICATION = 'DjangoGirlsTutorial.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME': str(Path(BASE_DIR, 'db.sqlite3')),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'NAME': str(Path(BASE_DIR, 'db.sqlite3')),
     }
 }
 
